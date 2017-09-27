030dfcbfc9d339fbb154959d5c00ea8a1b2a784f
edriessen <erik.driessen@outlook.com>
2017-09-27 21:21:35 +0200

    Add credentials example file

---
+++ credentials-example.py
@@ -0,0 +1,8 @@
+# set credentials variables from Google developer console
+client_id = ''
+client_secret= ''
+redirect_uri = ''
+# set credentials for oauth connection by running config.py 2 times
+access_code = ''
+access_token = ''
+refresh_token = ''
